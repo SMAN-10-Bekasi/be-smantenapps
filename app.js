c3b17615ad29aa7e905feb32ec13ca88dee3a51c
fdhlnadli <fadhlansatrio15@gmail.com>
2022-10-18 18:58:45 +0700

    merubah port

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ const Classroom = require("./models/classroom");
 const Qrcode = require("./models/qrcode");
 
 const app = express();
+const port = 8000;
 
 app.use(express.json());
 app.use(studentRouter);
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send("<h1>Halo kamu mau ngapain?</h1>");
 });
 
-app.listen(8000, () => {
-  console.log("Port is listening.");
+app.listen(port, () => {
+  console.log(`Rest API is listening at http://localhost:${port}..`);
 });
